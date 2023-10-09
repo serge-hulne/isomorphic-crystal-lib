7db3a2889d443f721894b865eff3620017c5a3dd
serge <serge.hulne@gmail.com>
2023-10-09 15:09:37 +0200

    Linux

--- src/gui/run.cr
+++ src/gui/run.cr
@@ -1,15 +1,3 @@
-# require "webview"
-# def run_app(title : String)
-#   puts "http://#{IP}:#{PORT}/#{ROOT}"
-
-#   wv = Webview.window(WIDTH, HEIGHT, Webview::SizeHints::NONE,
-#     "#{title}",
-#     "http://#{IP}:#{PORT}/#{ROOT}")
-
-#   wv.run
-#   wv.destroy
-# end
-
 
 {% if flag?(:linux) %}
   # Linux
