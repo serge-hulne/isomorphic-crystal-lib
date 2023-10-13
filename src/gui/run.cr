2d16809b0cb0268585aa1b818ea26b2ec6e96af8
serge <serge.hulne@gmail.com>
2023-10-13 18:12:40 +0200

    Renaming repo

--- src/gui/run.cr
+++ src/gui/run.cr
@@ -14,31 +14,11 @@
     gets
   end
 
-  # require "process"
-  # def run_app(title : String)
-  #   puts "http://#{IP}:#{PORT}/#{ROOT}"
-  #   sleep(2) # Wait a bit for the server to start
-  #   #Process.new("/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",  args: ["--app=http://#{IP}:#{PORT}/#{ROOT}"])
-  #   Process.new("google-chrome",  args: ["--app=http://#{IP}:#{PORT}/#{ROOT}"])
-  #   puts "Press Enter to close..."
-  #   gets
-  # end
-
 {% elsif flag?(:darwin) %}
-  
+
   #======
   # Mac
   #======
-  
-  # require "webview"
-  # def run_app(title : String)
-  #   puts "http://#{IP}:#{PORT}/#{ROOT}"
-  #   wv = Webview.window(WIDTH, HEIGHT, Webview::SizeHints::NONE,
-  #     "#{title}",
-  #     "http://#{IP}:#{PORT}/#{ROOT}")
-  #   wv.run
-  #   wv.destroy
-  # end
 
   require "process"
   def run_app(title : String)
@@ -49,19 +29,51 @@
     gets
   end
 
-
 {% elsif flag?(:win32) %}
   
   #========
   # Windows
   #========
   
-  def run_app(title : String)
-    puts "http://#{IP}:#{PORT}/#{ROOT}"
-    sleep(1) # Wait a bit for the server to start
-    Process.new("start chrome",  args: ["--app=http://#{IP}:#{PORT}/#{ROOT}"])
-    puts "Press Enter to close..."
-    gets
-  end
+  #
+  # Not implemented yet.
+  #
 
 {% end %}
+
+
+
+# Using Chrome
+##############
+
+# Windows:
+# -------
+# def run_app(title : String)
+#   puts "http://#{IP}:#{PORT}/#{ROOT}"
+#   sleep(1) # Wait a bit for the server to start
+#   Process.new("start chrome",  args: ["--app=http://#{IP}:#{PORT}/#{ROOT}"])
+#   puts "Press Enter to close..."
+#   gets
+# end
+
+# Mac OS
+# ------
+# require "process"
+# def run_app(title : String)
+#   puts "http://#{IP}:#{PORT}/#{ROOT}"
+#   sleep(1) # Wait a bit for the server to start
+#   #Process.new("/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",  args: ["--app=http://#{IP}:#{PORT}/#{ROOT}"])
+#   Process.new("google-chrome",  args: ["--app=http://#{IP}:#{PORT}/#{ROOT}"])
+#   puts "Press Enter to close..."
+#   gets
+# end
+
+# Linux
+# -----
+# def run_app(title : String)
+#   puts "http://#{IP}:#{PORT}/#{ROOT}"
+#   sleep(1) # Wait a bit for the server to start
+#   Process.new("google-chrome",  args: ["--app=http://#{IP}:#{PORT}/#{ROOT}"])
+#   puts "Press Enter to close..."
+#   gets
+# end
