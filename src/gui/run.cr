42a9b25dca7468f9a3360c3cae4f187d9ebdc0a3
serge <serge.hulne@gmail.com>
2023-10-16 12:23:55 +0200

    run() : Command-line options for the browser

--- src/gui/run.cr
+++ src/gui/run.cr
@@ -1,3 +1,4 @@
+require "./settings"
 
 {% if flag?(:linux) %}
   
@@ -6,13 +7,26 @@
   #======
   
   require "process"
-  def run_app(title : String)
-    puts "http://#{IP}:#{PORT}/#{ROOT}"
+
+  def run_app(title : String, width : Int32, height : Int32)
+    puts "http://#{IP}:#{PORT}/#{ROOT}"  
     sleep(1) # Wait a bit for the server to start
-    Process.new("isomorphic",  args: ["http://#{IP}:#{PORT}/#{ROOT}"])
+    
+    # Arguments array
+    args = [
+      "-p", "http://#{IP}:#{PORT}/#{ROOT}",
+      "-t", title,
+      "-w", width.to_s,
+      "-h", height.to_s
+    ]
+
+    # Start the external process
+    process = Process.run("isomorphic", args)
+
     puts "Press Enter to close..."
     gets
   end
+
 
 {% elsif flag?(:darwin) %}
 
@@ -21,13 +35,26 @@
   #======
 
   require "process"
-  def run_app(title : String)
-    puts "http://#{IP}:#{PORT}/#{ROOT}"
+  
+  def run_app(title : String, width : Int32, height : Int32)
+    puts "http://#{IP}:#{PORT}/#{ROOT}"  
     sleep(1) # Wait a bit for the server to start
-    Process.new("isomorphic",  args: ["http://#{IP}:#{PORT}/#{ROOT}"])
+    
+    # Arguments array
+    args = [
+      "-p", "http://#{IP}:#{PORT}/#{ROOT}",
+      "-t", title,
+      "-w", width.to_s,
+      "-h", height.to_s
+    ]
+
+    # Start the external process
+    process = Process.run("isomorphic", args)
+
     puts "Press Enter to close..."
     gets
   end
+
 
 {% elsif flag?(:win32) %}
   
